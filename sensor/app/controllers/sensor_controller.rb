f93b3b9cfbf8ccf5fde5107a228496a0650f7f1c
jlhonora <jlhonora@ing.puc.cl>
2014-11-09 22:06:31 -0300

    Setting variabls with JSON format

--- sensor/app/controllers/sensor_controller.rb
+++ sensor/app/controllers/sensor_controller.rb
@@ -1,4 +1,5 @@
 class SensorController < ApplicationController
   def index
+    @measurements_json = Measurements.where(created_at: Time.now.beginning_of_day..Time.now.end_of_day).to_json
   end
 end
