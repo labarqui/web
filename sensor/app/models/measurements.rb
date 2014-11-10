d6b34d10129b9cb4112b24ed95fb019d21c0ae1b
jlhonora <jlhonora@ing.puc.cl>
2014-11-09 22:04:58 -0300

    Mods to measurements to include param parsing

--- sensor/app/models/measurements.rb
+++ sensor/app/models/measurements.rb
@@ -3,16 +3,16 @@ class Measurements < ActiveRecord::Base
   def self.create_from_params(params)
     return if params.blank?
 
-    success = false
+    success = 0
 
     temp = params[:temperatura]
     if !temp.blank?
-      success ||= Measurements.create(value: temp, sensor_type: 0)
+      success += 1 if Measurements.create(value: temp, sensor_type: 0)
     end
 
     hum = params[:humedad]
     if !hum.blank?
-      success ||= Measurements.create(value: hum, sensor_type: 1)
+      success += 1 if Measurements.create(value: hum, sensor_type: 1)
     end
 
     return success
