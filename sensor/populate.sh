e1a3692e222d111baa2fbf2002574b864a936950
jlhonora <jlhonora@ing.puc.cl>
2014-11-09 22:03:00 -0300

    Simple script to add data

---
+++ sensor/populate.sh
@@ -0,0 +1,4 @@
+curl -X POST -H "Content-Type: application/json" -d '{"temperatura": 20.4, "humedad":40.0}' http://localhost:3000/api/sensor/create
+curl -X POST -H "Content-Type: application/json" -d '{"temperatura": 23.1, "humedad":45.9}' http://localhost:3000/api/sensor/create
+curl -X POST -H "Content-Type: application/json" -d '{"temperatura": 19.6, "humedad":46.3}' http://localhost:3000/api/sensor/create
+curl -X POST -H "Content-Type: application/json" -d '{"temperatura": 18.6, "humedad":38.7}' http://localhost:3000/api/sensor/create
